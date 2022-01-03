d8cd6634efc32cd45cf97e02435c23e73f122383
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-01-03 20:53:56 +0300

    Add hover to all portfolio items successfuly

--- js/scripts.js
+++ js/scripts.js
@@ -16,28 +16,15 @@ $(document).ready(function() {
   });
 
   //add hover effect on portfolio items
-  // $(".portfolio-item").hover(function() {
-  //   $(".portfolio-image").addClass(".portfolio-image--opacity");
-  // }, function() {
-  //   $(".portfolio-image").removeClass(".portfolio-image--opacity");
-  // });
-  $(".portfolio-item-1").hover(function() {
-    $(".text-overlay-1").removeClass("hover-hidden");
-    $(".text-overlay-1").addClass("hover");
-    $(".portfolio-image-1").addClass("portfolio-image--opacity");
-  }, function() {
-    $(".text-overlay-1").removeClass("hover");
-    $(".text-overlay-1").addClass("hover-hidden");
-    $(".portfolio-image-1").removeClass("portfolio-image--opacity");
-  });
-
-  $(".portfolio-item-2").hover(function() {
-    $(".text-overlay-2").removeClass("hover-hidden");
-    $(".text-overlay-2").addClass("hover");
-    $(".portfolio-image-2").addClass("portfolio-image--opacity");
-  }, function() {
-    $(".text-overlay-2").removeClass("hover");
-    $(".text-overlay-2").addClass("hover-hidden");
-    $(".portfolio-image-2").removeClass("portfolio-image--opacity");
-  });
+  for (let i = 1; i < 9; i++) {
+    $(".portfolio-item-" + i).hover(function() {
+      $(".text-overlay-" + i).removeClass("hover-hidden");
+      $(".text-overlay-" + i).addClass("hover");
+      $(".portfolio-image-" + i).addClass("portfolio-image--opacity");
+    }, function() {
+      $(".text-overlay-" + i).removeClass("hover");
+      $(".text-overlay-" + i).addClass("hover-hidden");
+      $(".portfolio-image-" + i).removeClass("portfolio-image--opacity");
+    });
+  }
 });
