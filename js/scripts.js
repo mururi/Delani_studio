5afdd799f6ad6d849744e113b4c96243b47863b7
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-01-03 18:53:19 +0300

    Attempt implementing hover effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-//toggle what we do descriptions
 $(document).ready(function() {
+  //toggle "what we do" section descriptions
   $(".design").click(function() {
     $("#design-image").toggle();
     $("#design-description").toggle();
@@ -13,5 +13,21 @@ $(document).ready(function() {
   $(".prod").click(function() {
     $("#prod-image").toggle();
     $("#prod-description").toggle();
+  });
+
+  //add hover effect on portfolio items
+  // $(".portfolio-item").hover(function() {
+  //   $(".portfolio-image").addClass(".portfolio-image--opacity");
+  // }, function() {
+  //   $(".portfolio-image").removeClass(".portfolio-image--opacity");
+  // });
+  $(".portfolio-item").hover(function() {
+    $(".text-overlay").removeClass("hover-hidden");
+    $(".text-overlay").addClass("hover");
+    $(".portfolio-image").addClass("portfolio-image--opacity");
+  }, function() {
+    $(".text-overlay").removeClass("hover");
+    $(".text-overlay").addClass("hover-hidden");
+    $(".portfolio-image").removeClass("portfolio-image--opacity");
   });
 });
